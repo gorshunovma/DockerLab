38c3f8d9990e6884b69d02e0a4fe6a12eeb140d6
gorshunovma <gorshunov.ma>
2024-08-15 05:51:23 +0300

    02-20240814

--- postgresql/entrypoint.sh
+++ postgresql/entrypoint.sh
@@ -1,9 +1,9 @@
  
-#!/bin/bash
+#!/bin/sh
 
 function postgres_start()
 {
-    sudo su postgres -c "pg_ctlcluster 16 main start"
+    su postgres -c "pg_ctlcluster 16 main start"
     until pg_isready -h localhost -p 5432
     do
         echo "Waiting for PostgreSQL to start..."
@@ -11,16 +11,20 @@ function postgres_start()
     done
     echo "PostgreSQL has been initialized."
 }
+
 chown 1111:1111 /var/lib/postgresql /var/log/postgresql /etc/postgresql
-if test -z "$(ls -A /var/lib/postgresql)"; then
-    sudo su postgres -c "pg_createcluster 16 main"
+
+if su postgres -c "[ ! -d '/var/lib/postgresql/16' ]"; then
+
+    su postgres -c "pg_createcluster 16 main"
     postgres_start
-    sudo su postgres -c "psql -c \"CREATE DATABASE zabbix;\""
-    sudo su postgres -c "psql -c \"CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
+    su postgres -c "psql -c \"CREATE DATABASE zabbix;\""
+    su postgres -c "psql -c \"CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
             CREATE USER zabbix WITH PASSWORD 'zabbix';
             GRANT ALL PRIVILEGES ON DATABASE zabbix TO zabbix;
             ALTER DATABASE zabbix OWNER TO zabbix;\""
 else
     postgres_start
 fi
-sudo su postgres -c "tail -f /dev/null"
+echo 'PostgreSQL Starting...'
+su postgres -c "tail -f /dev/null"
