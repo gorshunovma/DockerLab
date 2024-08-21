270bf990feae42d5ee13083fbf0c5263a40290c2
gorshunovma <gorshunov.ma@gmail.com>
2024-08-21 22:49:48 +0300

    end-21.08.2024 22:50

--- temp/rebuild.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-docker-compose rm -f
-docker rm -f $(docker ps -a)
-sudo chown -R astra:astra ./postgresql/ ./pgdata
-rm -Rfv ./postgresql/data/* ./postgresql/log/*  ./postgresql/etc/* ./pgdata/*
-rm -Rfv ./postgresql/data/* ./postgresql/log/ ./postgresql/etc/ ./pgdata/
